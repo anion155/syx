764283c26f52460ad4273080d569a39b546bdf11
Mikhailov Anton <anion155@gmail.com>
2026-04-17 18:22:10 +0400

    fix: build

--- nob.c
+++ nob.c
@@ -4,7 +4,7 @@
 #define NOB_IMPLEMENTATION
 #include "./vendor/nob.h"
 #undef NOB_IMPLEMENTATION
-#include "./vendor/nob_main.h"
+#include "./vendor/help.h"
 
 #define SRC_DIR "./src"
 #define VENDOR_DIR "./vendor"
@@ -14,34 +14,34 @@ Nob_Cmd cmd = {0};
 bool *flag_run;
 bool *flag_debug;
 
-bool define_args() {
-  flag_run   = flag_bool("run", false,  "Run result, when provided arguments after '--'");
-  flag_debug = flag_bool("g", false,    "Build with debugger support");
-  return true;
-}
-
-bool build_main(int argc, char **argv) {
+bool build(int argc, char **argv) {
   if (!nob_mkdir_if_not_exists(BUILD_DIR)) return false;
 
   nob_cc(&cmd);
   nob_cc_flags(&cmd);
-  #if defined(__APPLE__)
-    nob_cmd_append(&cmd, "-framework", "Cocoa", "-framework", "AudioToolbox");
-  #else
-    nob_cmd_append(&cmd, "-lX11", "-lasound");
-  #endif
   nob_cmd_append(&cmd, "-I"SRC_DIR);
   nob_cmd_append(&cmd, "-I"VENDOR_DIR);
-  if (*flag_debug) nob_cmd_append(&cmd, "-g");
+  if (*flag_debug) nob_cmd_append(&cmd, "-ggdb");
+  nob_cc_inputs(&cmd, "-std=c23");
   nob_cc_inputs(&cmd, SRC_DIR"/main.c");
-  nob_cc_output(&cmd, BUILD_DIR"/bogo");
+  nob_cc_output(&cmd, BUILD_DIR"/sexpr");
   if (!nob_cmd_run_opt(&cmd, (Nob_Cmd_Opt){})) return false;
 
   if (*flag_run) {
-    nob_cmd_append(&cmd, BUILD_DIR"/bogo");
+    nob_cmd_append(&cmd, BUILD_DIR"/sexpr");
     nob_da_append_many(&cmd, argv, argc);
     if (!nob_cmd_run(&cmd)) return false;
   }
 
   return true;
+}
+
+int main(int argc, char **argv) {
+  NOB_GO_REBUILD_URSELF(argc, argv);
+
+  flag_run   = flag_bool("run", false,  "Run result, when provided arguments after '--'");
+  flag_debug = flag_bool("g", false,    "Build with debugger support");
+  flag_parse_with_help(&argc, &argv);
+  if (!build(argc, argv)) return 1;
+  return 0;
 }
--- vendor/help.h
+++ vendor/help.h
@@ -1,34 +1,23 @@
 #include "./flag.h"
 #include "./nob.h"
 
-#pragma once
-
-bool define_args();
-bool build_main(int argc, char **argv);
-
 void usage(FILE *stream) {
   fprintf(stream, "usage: ./nob [<options>] [--]\n");
   fprintf(stream, "options:\n");
   flag_print_options(stream);
 }
 
-int main(int argc, char **argv) {
-  NOB_GO_REBUILD_URSELF(argc, argv);
-
+void flag_parse_with_help(int *argc, char ***argv) {
   bool *flag_help = flag_bool("help", false, "Print this help to stdout and exit with 0");
-  if (!define_args()) return 1;
-  if (!flag_parse(argc, argv)) {
+  if (!flag_parse(*argc, *argv)) {
     usage(stderr);
     flag_print_error(stderr);
-    return 1;
+    exit(1);
   }
   if (*flag_help) {
     usage(stdout);
-    return 0;
+    exit(0);
   }
-  argc = flag_rest_argc();
-  argv = flag_rest_argv();
-
-  if (!build_main(argc, argv)) return 1;
-  return 0;
+  *argc = flag_rest_argc();
+  *argv = flag_rest_argv();
 }
