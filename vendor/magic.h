b6a61010611d9146e0395429f69f659e729cb09d
Mikhailov Anton <anion155@gmail.com>
2026-05-02 21:42:18 +0400

    chore: move things

--- vendor/magic.h
+++ vendor/magic.h
@@ -9,4 +9,6 @@
 #define WITH_DEFAULT(d, ...) SECOND_ARG(__VA_OPT__(, ) __VA_ARGS__, d)
 #define WITH_TWO_DEFAULTS(d1, d2, ...) SECOND_ARG(dummy __VA_OPT__(, __VA_ARGS__), d1), THIRD_ARG(dummy __VA_OPT__(, __VA_ARGS__), d2, d2)
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 #endif // MAGIC_H_
