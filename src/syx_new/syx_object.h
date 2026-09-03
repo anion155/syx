ab918bb849fca595960da01dd8680e7cb9dff77e
Mikhailov Anton <anion155@gmail.com>
2026-09-03 23:08:33 +0400

    feat: update object api

--- src/syx_new/syx_object.h
+++ src/syx_new/syx_object.h
@@ -3,7 +3,9 @@
 
 #include <syx_new/syx_value.h>
 
+Syx_Value **syx_object_lookup(Syx_Object *object, Syx_Symbol *field_name);
 Syx_Value *syx_object_get(Syx_Eval_Ctx *ctx, Syx_Object *object, Syx_Symbol *field_name);
+void syx_object_define(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form);
 void syx_object_set(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form);
 void syx_object_delete(Syx_Object *object, Syx_Symbol *field_name);
 
@@ -18,8 +20,18 @@ void syx_object_delete(Syx_Object *object, Syx_Symbol *field_name);
 #define SYX_EVAL_IMPL
 #include <syx_new/syx_eval.h>
 
+Syx_Value **syx_object_lookup(Syx_Object *object, Syx_Symbol *field_name) {
+  Syx_Value **field;
+  while (!field && object) {
+    field = ht_find(&object->fields, field_name);
+    if (field) break;
+    object = object->proto;
+  }
+  return field;
+}
+
 Syx_Value *syx_object_get(Syx_Eval_Ctx *ctx, Syx_Object *object, Syx_Symbol *field_name) {
-  Syx_Value **field = ht_find(&object->fields, field_name);
+  Syx_Value **field = syx_object_lookup(object, field_name);
   if (!field) {
     return syx_value_nil();
   } else if ((*field)->kind == SYX_VALUE_KIND_PREFIXED && (*field)->prefixed->kind == SYX_PREFIXED_KIND_QUOTE) {
@@ -31,8 +43,8 @@ Syx_Value *syx_object_get(Syx_Eval_Ctx *ctx, Syx_Object *object, Syx_Symbol *fie
     return rc_move(result);
   }
 }
-void syx_object_set(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form) {
-  Syx_Value **field = ht_find(&object->fields, field_name);
+
+inline void syx_object_field_set(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form, Syx_Value **field) {
   if (!field) {
     rc_acquire(syx_value_from_symbol(field_name));
     field = ht_put(&object->fields, field_name);
@@ -42,6 +54,17 @@ void syx_object_set(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form)
     *field = rc_acquire(form);
   }
 }
+
+void syx_object_define(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form) {
+  Syx_Value **field = ht_find(&object->fields, field_name);
+  syx_object_field_set(object, field_name, form, field);
+}
+
+void syx_object_set(Syx_Object *object, Syx_Symbol *field_name, Syx_Value *form) {
+  Syx_Value **field = syx_object_lookup(object, field_name);
+  syx_object_field_set(object, field_name, form, field);
+}
+
 void syx_object_delete(Syx_Object *object, Syx_Symbol *field_name) {
   Syx_Value **field = ht_find(&object->fields, field_name);
   if (field) {
